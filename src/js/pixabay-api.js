87a412cc3bda083aea54afe7704e2c1a037a0d5f
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-04-20 19:19:24 +0100

    start

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,4 @@
+
+import iziToast from "izitoast";
+
+const API_KEY = "43490794-44764587ca88d9eedc273b8a9";
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import { PixabayApiService } from './js/pixabay-api.js';
+// import { createMarkup } from './js/render-functions.js';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
